1356775f3d5dbd055605d80793c7dbe2a732f76e
PragatiNeraki <pragatineraki60@gmail.com>
2025-01-11 20:24:16 +0530

    first commit

---
+++ frontend/src/service/config.tsx
@@ -0,0 +1,12 @@
+
+
+export const BASE_URL = "http://192.168.1.121:3000/api"
+export const SOCKET_URL = "http://192.168.1.121:3000"
+
+// export const BASE_URL = "http://192.168.1.4:3000/api"
+// export const SOCKET_URL = "http://192.168.1.4:3000"
+
+
+// export const BASE_URL = "http://localhost:3000/api"
+// export const SOCKET_URL = "http://localhost:3000"
+
